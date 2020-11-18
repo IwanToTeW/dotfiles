8cb947ecf262fc716b9fea50829eb31b1b3de171
IwanToTeW <ivantotev_96@abv.bg>
2020-11-18 16:42:56 +0000

    Added vs code extensions and bash aliases

---
+++ aliases.zsh
@@ -0,0 +1,7 @@
+#Laravel aliases
+alias art='php artisan'
+alias hor='clear && redis-cli flushdb && art horizon'
+
+#General aliases
+alias api='cd ~/Work/school-spider/api'
+alias portal='cd ~/Work/school-spider/portal'
